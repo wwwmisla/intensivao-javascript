2bf58d0e49568053cf21ed5515019596a14a836a
Misla Wislaine <mislawislaine12@gmail.com>
2024-02-02 20:59:12 -0300

    :sparkles: Projeto Base Pronto

---
+++ hashtaurante/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Hashtaurante from './assets/hashtaurante.webp';
+import './App.css';
+import { Navegacao } from './Navegacao';
+import { ItemCardapio } from './ItemCardapio';
+import { pratosPrincipais, sobremesas, bebidas } from './cardapio';
+
+export function App() {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
+  return <>
+          <img src={Hashtaurante} alt="" className='capa'/>
+          <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+          <div className='menu'>
+            {paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} preco={item.preco} descricao={item.descricao} imagem={item.imagem}/>)}
+          </div>
+        </>
+}
